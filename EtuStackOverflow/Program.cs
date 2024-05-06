a40761b7171cc268a7739e62262093ff4299e05d
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2024-05-06 20:05:10 +0300

    question page bitti ft. omer

--- EtuStackOverflow/Program.cs
+++ EtuStackOverflow/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
