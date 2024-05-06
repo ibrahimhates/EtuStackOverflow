673c92606de26180299e48013838b80bba040307
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2024-05-06 12:24:44 +0300

    swaggergen projeye test edebilmek için eklendi

--- EtuStackOverflow/Program.cs
+++ EtuStackOverflow/Program.cs
@@ -1,8 +1,9 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +12,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
