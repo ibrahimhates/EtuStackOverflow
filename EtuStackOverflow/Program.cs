1afd0e746f95689b68c8d00fb082cbd90d5e9a82
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2024-06-11 17:18:34 +0300

    pushing

--- EtuStackOverflow/Program.cs
+++ EtuStackOverflow/Program.cs
@@ -82,6 +82,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.Urls.Add("https://*:5004");
 }
 
 if (app.Environment.IsDevelopment())
