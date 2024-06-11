37b797ebe2bd5ec39196cf4dc72de7e2b70bfbe6
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2024-06-11 19:08:32 +0300

    test1

--- EtuStackOverflow/Program.cs
+++ EtuStackOverflow/Program.cs
@@ -79,7 +79,6 @@ app.AddMigration();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
